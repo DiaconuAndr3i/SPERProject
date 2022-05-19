7fa95501209b88c270662fd2bd01149056b303a3
Andrei Diaconu <diaconu.andrei99@gmail.com>
2022-05-19 12:52:22 +0300

    Comments code in BUG0.py file

--- BUG0.py
+++ BUG0.py
@@ -1,19 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# Clasa in care mapam atat universul cat si functionalitatea
+# in care isi desfasoara activitatea robotul in urma aplicarii algoritmului
 class Bug:
     def __init__(self, start_point_x, start_point_y, goal_point_x,
                  goal_point_y, obstacle_x, obstacle_y):
+        # initializam punctul de sosire
         self.goal_point_x = goal_point_x
         self.goal_point_y = goal_point_y
+
+        # initializam obstacolele
         self.obstacle_x = obstacle_x
         self.obstacle_y = obstacle_y
+
+        # initializam drumul pe care il va parcurge robotul
         self.road_x = [start_point_x]
         self.road_y = [start_point_y]
-        self.out_x = []
-        self.out_y = []
 
+        # initializam frontiera obstacolelor
+        self.boundaryOfObstacle_x = []
+        self.boundaryOfObstacle_y = []
+
+
+        # stabilim frontiera pentru fiecare obstacol furnizat
         for item_obstacle, jtem_obstacle in zip(obstacle_x, obstacle_y):
             """
                   1
@@ -22,53 +32,72 @@ class Bug:
             """
             for add_direction_x, add_direction_y in zip([-1, -1, -1, 0, 0, 1, 1, 1],
                                                         [-1, 0, 1, -1, 1, -1, 0, 1]):
-                poz_x = item_obstacle + add_direction_x
-                poz_y = jtem_obstacle + add_direction_y
+                poz_x = item_obstacle + add_direction_x # alegem un posibil punct de pe frontiera in raport cu x
+                poz_y = jtem_obstacle + add_direction_y # similar pentru y
                 ok = True
                 for item_obstacle_2, jtem_obstacle_2 in zip(obstacle_x, obstacle_y):
                     if poz_x == item_obstacle_2 and poz_y == jtem_obstacle_2:
                         ok = False
                         break
                 if ok:
-                    self.out_x.append(poz_x)
-                    self.out_y.append(poz_y)
+                    self.boundaryOfObstacle_x.append(poz_x)  # daca punctul chiar se gaseste
+                                                            # pe frontiera il adaugam in vectorul corespunzator
+                    self.boundaryOfObstacle_y.append(poz_y)
 
+
+    # Realizam o miscare simpla spre punctul de sosire in conditiile in care nu suntem ingraditi de vreun obstacol
+    # de care ne-am putea lovi in imediata apropiere
     def simpleMove(self):
         return self.road_x[-1] + np.sign(self.goal_point_x - self.road_x[-1]), \
                self.road_y[-1] + np.sign(self.goal_point_y - self.road_y[-1])
 
+    # Realizam o miscare pe frontiera obstacolului in punctele inca nevizitate
+    # daca este posibila
     def moveToNextObstacle(self, visited_x, visited_y):
-        for on_axes_move_x, on_axes_move_y in zip([1, 0, -1, 0],
-                                                  [0, 1, 0, -1]):
+        """
+        runCase3 - Poate genera probleme de gasire a destinatiei:
+            - Pentru cazul de mai jos din for va gasi destinatia fara probleme
+            - Pentru urmatorul caz,
+                [-1, 0, 1, 0]
+                [0, 1, 0, -1],
+            de parcurgere a frontierei obstacolului, algoritmul se va bloca ajungand in starea de unde a pornit
+            OBS: Limitare a BUG0
+        """
+        for on_axes_move_x, on_axes_move_y in zip([-1, 0, 1, 0],  # Facem o miscare ori pe orizontala ori pe verticala datorita
+                                                                  # naturii obstacolelor
+                                                  [0, -1, 0, 1]):
             move_x = self.road_x[-1] + on_axes_move_x
             move_y = self.road_y[-1] + on_axes_move_y
 
-            for x, y in zip(self.out_x, self.out_y):
+            for x, y in zip(self.boundaryOfObstacle_x, self.boundaryOfObstacle_y):
                 pct_ok = True
-                if move_x == x and move_y == y:
+                if move_x == x and move_y == y:  # Incercam sa gasim un nou punct de pe frontiera nevizitat daca exista
                     for item, jtem in zip(visited_x, visited_y):
                         if move_x == item and move_y == jtem:
                             pct_ok = False
                             break
                     if pct_ok:
-                        return move_x, move_y
+                        return move_x, move_y # Returnam punctul nevizitat
                 if not pct_ok:
                     break
 
-        return self.road_x[-1], self.road_y[-1]
+        return self.road_x[-1], self.road_y[-1] # Daca nu exista punct nevizitat ramanem in punctul curent al robotului
 
     def bug0(self):
+        # Presupunem ca pornim cu o miscare simpla ( Nu avem obstacole in imediata vecinatate a punctului de miscare )
         move_dir = 'simpleMove'
 
         move_x, move_y = -np.inf, -np.inf
 
+        # Plotam mapa la momentul initial de timp
         plt.plot(self.obstacle_x, self.obstacle_y, ".k")
         plt.plot(self.road_x[-1], self.road_y[-1], "o")
         plt.plot(self.goal_point_x, self.goal_point_y, "x")
-        plt.plot(self.out_x, self.out_y, ".")
+        plt.plot(self.boundaryOfObstacle_x, self.boundaryOfObstacle_y, ".")
         plt.grid(True)
 
-        for ob_x, ob_y in zip(self.out_x, self.out_y):
+        # Daca robotul porneste din vecinatatea unui obstacol ( se afla pe frontiera ) schimbam tipul de deplasare
+        for ob_x, ob_y in zip(self.boundaryOfObstacle_x, self.boundaryOfObstacle_y):
             if self.road_x[-1] == ob_x and self.road_y[-1] == ob_y:
                 move_dir = "moveToNextObstacle"
                 break
@@ -77,17 +106,21 @@ class Bug:
         visited_y = []
 
         while True:
+            # Daca ajungem la destinatie
             if self.road_x[-1] == self.goal_point_x and self.road_y[-1] == self.goal_point_y:
                 print('I have reached my destination')
                 break
 
             if move_dir == "simpleMove":
+                # Ne pozitionam in noul punct rezultat dintr-o miscare simpla
                 move_x, move_y = self.simpleMove()
             if move_dir == "moveToNextObstacle":
+                # Ne pozitionam in noul punct rezultat dintr-o miscare pe frontiera
                 move_x, move_y = self.moveToNextObstacle(visited_x, visited_y)
             if move_dir == "simpleMove":
                 boundary = False
-                for ob_x, ob_y in zip(self.out_x, self.out_y):
+                # Daca venim dintr-o miscare simpla iar noul punct este pe frontiera semnalam acest lucru
+                for ob_x, ob_y in zip(self.boundaryOfObstacle_x, self.boundaryOfObstacle_y):
                     if move_x == ob_x and move_y == ob_y:
                         self.road_x.append(move_x)
                         self.road_y.append(move_y)
@@ -95,19 +128,26 @@ class Bug:
                         visited_y[:] = []
                         visited_x.append(move_x)
                         visited_y.append(move_y)
+                        # Schimbam tipul de miscare in miscare pe frontiera
                         move_dir = "moveToNextObstacle"
                         boundary = True
                         break
+                # Altfel, continuam cu miscarea simpla si stabilim ca punctul este din drumul spre sosire
                 if not boundary:
                     self.road_x.append(move_x)
                     self.road_y.append(move_y)
+            # Altfel, avem o deplasare pe frontiera
             elif move_dir == "moveToNextObstacle":
+                # Incercam o miscare simpla din acel punct (urmam scopul, acela de a ajunge la goal)
                 normal_move = True
                 for ob_x, ob_y in zip(self.obstacle_x, self.obstacle_y):
                     simpleMoveRet = self.simpleMove()
+                    # Daca lovim un punct din obstacol, continuam ocolirea acestuia
                     if simpleMoveRet[0] == ob_x and simpleMoveRet[1] == ob_y:
                         normal_move = False
                         break
+                # Daca punctul rezultat din deplasarea printr-o miscare simpla nu intersecteaza un obstacol marcam ca putem
+                # face o astfel de miscare
                 if normal_move:
                     move_dir = "simpleMove"
                 else:
@@ -121,7 +161,7 @@ class Bug:
 
         plt.show()
 
-
+# Construim obstacolele prin furnizarea limitelor de intindere
 def constructObstacles(limit_obstacle_x, limit_obstacle_y):
     obstacle_x = []
     obstacle_y = []
@@ -134,7 +174,7 @@ def constructObstacles(limit_obstacle_x, limit_obstacle_y):
 
     return obstacle_x, obstacle_y
 
-
+# Configuratie mapa 1
 def runCase1():
     start_point_x = 0.0
     start_point_y = 190.0
@@ -147,7 +187,7 @@ def runCase1():
 
     return start_point_x, start_point_y, goal_point_x, goal_point_y, limit_obstacle_x, limit_obstacle_y
 
-
+# Configuratie mapa 2
 def runCase2():
     start_point_x = 170.0
     start_point_y = 40.0
@@ -160,7 +200,7 @@ def runCase2():
 
     return start_point_x, start_point_y, goal_point_x, goal_point_y, limit_obstacle_x, limit_obstacle_y
 
-
+# Configuratie mapa 3
 def runCase3():
     start_point_x = 20.0
     start_point_y = 140.0
@@ -175,7 +215,7 @@ def runCase3():
 
 
 if __name__ == "__main__":
-    start_point_x, start_point_y, goal_point_x, goal_point_y, limit_obstacle_x, limit_obstacle_y = runCase3()
+    start_point_x, start_point_y, goal_point_x, goal_point_y, limit_obstacle_x, limit_obstacle_y = runCase2()
 
     obstacle_x, obstacle_y = constructObstacles(limit_obstacle_x, limit_obstacle_y)
 
